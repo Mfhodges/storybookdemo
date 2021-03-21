28fe079ae1bdaf7ce7ec66975153669d2a9a2a25
mfhodges <mfhodges95@gmail.com>
2021-03-21 15:38:50 -0400

    finished badge component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,7 @@
 
+// Import the global style enabling tailwind classes
+import '../src/assests/tailwindcss.min.css';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,39 +1,40 @@
 import React from "react";
 
-////Users/maddyhodges/Desktop/CCFLStorybook/relay-component-library/src/assests/icons/remove.svg
 /**
  * Functional Component to display Tailwind Badge
  * @param {string} text - text label to display in the badge
  * @param {string} color - color family used to define the tailwind css classes
  */
-const Badge = ({ text, color, onRemove }) => {
+const Badge = ({ text, color, onRemove, isRemovable = false, isSmall=true }) => {
+  const badgePadding = isRemovable ? "px-2" : "px-3";
+  const textStyling = isSmall ? 'text-xs' : 'text-sm';
   return (
     <button
       href="#"
-      className={`bg-${color}-100 text-${color}-800 inline-block rounded-full font-medium text-xs mr-1 pl-3 pr-2 py-1`}
+      className={`bg-${color}-100 text-${color}-800 inline-block rounded-full font-medium text-xs mr-1 ${badgePadding} py-1`}
     >
-    <span>
-      {text}
-      </span>
-      <span
-        onClick={onRemove}
-        className={`inline-flex items-center justify-center w-4 h-4 ml-2 rounded-full text-${color}-400 hover:bg-${color}-200 hover:text-${color}-500`}
-      >
-        <svg
-          className="w-2.5 h-2.5 fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+      <span className={`align-middle ${textStyling}`}>{text}</span>
+      {isRemovable && (
+        <span
+          onClick={onRemove}
+          className={`inline-flex items-center align-middle justify-center w-4 h-4 ml-1 rounded-full text-${color}-400 hover:bg-${color}-200 hover:text-${color}-500`}
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </span>
+          <svg
+            className="w-2.5 h-2.5 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2.5"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </span>
+      )}
     </button>
   );
 };
