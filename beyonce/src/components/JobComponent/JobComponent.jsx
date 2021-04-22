8b2c6b02ffaefcf377fa3a1dc4034651e800f0d8
mfhodges <mfhodges95@gmail.com>
2021-04-22 14:26:21 -0400

    Finished job component :tada:

--- beyonce/src/components/JobComponent/JobComponent.jsx
+++ beyonce/src/components/JobComponent/JobComponent.jsx
@@ -1,54 +1,48 @@
 import React from "react";
 import ThumbUp from "../Thumb/ThumbUp";
 import ThumbDown from "../Thumb/ThumbDown";
+import Badge from "../Badge/Badge";
 
 // This Component is used in the Job Modal and in the first 5 Jobs
 
 /**
  *
- * @param {text} title - Job title
+ * @param {object} job - Job object
  * @param {text} children - Raw text of job description
  *
  */
 
 const JobComponent = ({ job, children }) => {
   return (
-    <div className="bg-white shadow rounded-lg divide-y divide-gray-200">
-      <div className="px-4 py-5 sm:px-6">
-        <div className="-ml-4 -mt-2 flex items-center justify-between flex-nowrap">
-          <div className="ml-4 mt-2">
-            <h3 className="text-lg leading-6 font-medium text-gray-900">
-              {job.title}
-            </h3>
-          </div>
+    <div className="bg-white shadow rounded-lg h-full">
+      <div className="bg-white px-10 py-5 border-b border-gray-200">
+        <div className="flex items-center justify-between flex-nowrap">
+          <h3 className="text-3xl leading-6 font-medium text-gray-900">
+            {job.title}
+          </h3>
           <div className="ml-4 mt-2 flex-shrink-0 flex w-28 justify-between">
             <ThumbUp /> <ThumbDown />
           </div>
         </div>
+        <div className="flex items-center justify-center sm:flex-nowrap sm:flex-row flex-col flex-wrap text-base text-gray-500">
+          <span>{job.status}</span>{" "}
+          <span className="hidden sm:px-4 sm:block text-gray-200">|</span>
+          <div>
+            {job.skills.map((skill) => (
+              <Badge text={skill}></Badge>
+            ))}
+          </div>{" "}
+          <span className="hidden sm:px-4 sm:block text-gray-200">|</span>
+          <span>{job.location}</span>
+        </div>
       </div>
       <div className="px-4 py-5 sm:p-6">
-      <div class="flex-1 flex bg-gray-100 overflow-hidden">
-      <div class="px-6 py-4 flex-1 overflow-y-scroll">
-            {children}</div>
-            </div>
+        <div className="m-px flex h-4/6 max-h-96 border-2 border-gray-300 rounded-lg">
+          <div className="px-6 py-4 overflow-y-auto">{children}</div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default JobComponent;
-
-{
-  /* <div className="rounded-lg text-center overflow-hidden shadow-xl transform transition-all">
-    <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
-      <div className="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap">
-        <div className="ml-4 mt-2">
-          <h3 className="text-lg leading-6 font-medium text-gray-900">{title}</h3>
-        </div>
-        <div className="ml-4 mt-2 flex-shrink-0">
-          <ThumbUp/> <ThumbDown/>
-        </div>
-      </div>
-    </div>
-    </div> */
-}
--- beyonce/src/components/JobComponent/JobComponent.stories.jsx
+++ beyonce/src/components/JobComponent/JobComponent.stories.jsx
@@ -1,29 +1,35 @@
-import React from 'react';
-import JobComponent from './JobComponent';
+import React from "react";
+import JobComponent from "./JobComponent";
+import Layout from "../Layout/Layout";
 
 const loremIpsem = `
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ligula ex. Nam sed nisi tincidunt, bibendum enim non, luctus nisl. Etiam sed egestas purus. Cras eleifend condimentum velit, non elementum lorem volutpat eu. Aliquam elementum augue a commodo varius. Aliquam porta placerat aliquam. Curabitur euismod eros dolor, sed venenatis arcu tempus at. Sed convallis iaculis erat, ut porttitor mauris fringilla et. Fusce volutpat condimentum est, et suscipit turpis condimentum lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam rhoncus pulvinar libero a gravida. Vivamus lobortis vehicula sagittis. Aliquam nibh nulla, imperdiet eleifend ipsum sed, molestie vehicula felis. Cras a elit lobortis, feugiat lacus nec, lobortis velit.
-<b>
 Phasellus fringilla iaculis turpis nec lacinia. Nam accumsan libero nisi, et dapibus ex pulvinar vitae. Morbi aliquam felis id metus condimentum, id elementum enim ornare. Integer tincidunt imperdiet nulla, sed scelerisque leo efficitur eget. Nulla porttitor faucibus mi euismod tincidunt. Ut eleifend lacus vel scelerisque semper. Donec lectus elit, mattis eget ante et, efficitur facilisis urna. Vestibulum vitae diam a ligula efficitur accumsan at vitae mauris. Nunc rutrum ullamcorper odio, sed imperdiet lectus fermentum dapibus.
-<b>
 Vivamus imperdiet a lorem malesuada condimentum. Phasellus mollis nibh id diam blandit ultricies. Nunc laoreet ex gravida, imperdiet ante interdum, suscipit neque. Aenean vestibulum dui erat, eu laoreet lectus accumsan vitae. Curabitur sed faucibus nisi, quis pellentesque lacus. Aliquam condimentum rhoncus dapibus. Nullam tincidunt ullamcorper mauris, vel auctor nibh. Cras venenatis dui in faucibus finibus. Donec placerat porta lorem, quis blandit sapien aliquet sed. Nulla fringilla eleifend feugiat. Sed id maximus eros, eu malesuada neque. In tincidunt sapien at libero hendrerit, vel malesuada felis maximus.
-`
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ligula ex. Nam sed nisi tincidunt, bibendum enim non, luctus nisl. Etiam sed egestas purus. Cras eleifend condimentum velit, non elementum lorem volutpat eu. Aliquam elementum augue a commodo varius. Aliquam porta placerat aliquam. Curabitur euismod eros dolor, sed venenatis arcu tempus at. Sed convallis iaculis erat, ut porttitor mauris fringilla et. Fusce volutpat condimentum est, et suscipit turpis condimentum lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam rhoncus pulvinar libero a gravida. Vivamus lobortis vehicula sagittis. Aliquam nibh nulla, imperdiet eleifend ipsum sed, molestie vehicula felis. Cras a elit lobortis, feugiat lacus nec, lobortis velit.
+Phasellus fringilla iaculis turpis nec lacinia. Nam accumsan libero nisi, et dapibus ex pulvinar vitae. Morbi aliquam felis id metus condimentum, id elementum enim ornare. Integer tincidunt imperdiet nulla, sed scelerisque leo efficitur eget. Nulla porttitor faucibus mi euismod tincidunt. Ut eleifend lacus vel scelerisque semper. Donec lectus elit, mattis eget ante et, efficitur facilisis urna. Vestibulum vitae diam a ligula efficitur accumsan at vitae mauris. Nunc rutrum ullamcorper odio, sed imperdiet lectus fermentum dapibus.
+Vivamus imperdiet a lorem malesuada condimentum. Phasellus mollis nibh id diam blandit ultricies. Nunc laoreet ex gravida, imperdiet ante interdum, suscipit neque. Aenean vestibulum dui erat, eu laoreet lectus accumsan vitae. Curabitur sed faucibus nisi, quis pellentesque lacus. Aliquam condimentum rhoncus dapibus. Nullam tincidunt ullamcorper mauris, vel auctor nibh. Cras venenatis dui in faucibus finibus. Donec placerat porta lorem, quis blandit sapien aliquet sed. Nulla fringilla eleifend feugiat. Sed id maximus eros, eu malesuada neque. In tincidunt sapien at libero hendrerit, vel malesuada felis maximus.
+`;
 
 export default {
-  title: 'Design System/Organisms/JobComponent',
+  title: "Design System/Organisms/JobComponent",
   component: JobComponent,
 };
 
-const Template = ({description,...args}) => {
-return <JobComponent{...args} >{loremIpsem}</JobComponent>};
+const Template = ({ description, content, ...args }) => {
+  return <JobComponent {...args}>{content}</JobComponent>;
+};
 
 export const Primary = Template.bind({});
 Primary.args = {
-    job: {
-        title: "Job Posting",
-        imageURL:"CodingfastlaneLogoFull.png",
-        company:"Company Name",
-        skills: ["React","Node","PostgreSQL"],
-        id:1,
-      },
+  content: loremIpsem,
+  job: {
+    title: "Job Posting",
+    status: "Full-Time",
+    imageURL: "CodingfastlaneLogoFull.png",
+    company: "Company Name",
+    location: "Philadelphia, PA",
+    skills: ["React", "Node", "PostgreSQL"],
+    id: 1,
+  },
 };
